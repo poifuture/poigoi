0a047dfa9865cae0c5927dee044a3bb94dcbb0f3
Nagi <nagi@poifuture.com>
2019-06-21 17:20:55 +0800

    Init Gatsby (#4)

---
+++ app/src/pages/index.js
@@ -0,0 +1,18 @@
+import React from "react"
+
+export default () => (
+  <div>
+    <ruby>
+      私<rt>わたし</rt>
+    </ruby>
+    たちの
+    <ruby>
+      夢<rt>ゆめ</rt>
+    </ruby>
+    がここから
+    <ruby>
+      始<rt>はじ</rt>
+    </ruby>
+    まります ٩(ˊᗜˋ*)و
+  </div>
+)
